c98f446344a1bd87e47ac6f10c5ca14a8ed1be2c
Denis Khalikov <dennis.khalikov@gmail.com>
2019-08-06 20:56:24 +0300

    Add feeds data

--- tools/mlir-vulkan-runner/mlir-vulkan-runner.cpp
+++ tools/mlir-vulkan-runner/mlir-vulkan-runner.cpp
@@ -39,10 +39,14 @@
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Support/StringExtras.h"
 #include "mlir/Translation.h"
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/ToolOutputFile.h"
 
+#include <cstdint>
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 #include <vulkan/vulkan.h>
 
 using namespace mlir;
@@ -109,7 +113,7 @@ static VkResult vkGetBestComputeQueueNPH(VkPhysicalDevice physicalDevice,
   return VK_ERROR_INITIALIZATION_FAILED;
 }
 
-static const VkInstance vulkanCreateInstance() {
+static VkInstance vulkanCreateInstance() {
   const VkApplicationInfo applicationInfo = {VK_STRUCTURE_TYPE_APPLICATION_INFO,
                                              0,
                                              "VKComputeSample",
@@ -146,7 +150,9 @@ static void processVariable(spirv::VariableOp varOp) {
   }
 }
 
-static void process(spirv::ModuleOp module) {
+static void processModule(spirv::ModuleOp module,
+                          std::unordered_map<int, std::vector<int32_t>> &vars) {
+
   for (auto &op : module.getBlock()) {
     if (isa<spirv::VariableOp>(op)) {
       processVariable(dyn_cast<spirv::VariableOp>(op));
@@ -157,17 +163,14 @@ static void process(spirv::ModuleOp module) {
   uint32_t physicalDeviceCount = 0;
   BAIL_ON_BAD_RESULT(
       vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, 0));
-
   VkPhysicalDevice *const physicalDevices = (VkPhysicalDevice *)malloc(
       sizeof(VkPhysicalDevice) * physicalDeviceCount);
-
   BAIL_ON_BAD_RESULT(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount,
                                                 physicalDevices));
-
-  for (uint32_t i = 0; i < physicalDeviceCount; i++) {
+  if (physicalDeviceCount) {
     uint32_t queueFamilyIndex = 0;
     BAIL_ON_BAD_RESULT(
-        vkGetBestComputeQueueNPH(physicalDevices[i], &queueFamilyIndex));
+        vkGetBestComputeQueueNPH(physicalDevices[0], &queueFamilyIndex));
 
     const float queuePrioritory = 1.0f;
     const VkDeviceQueueCreateInfo deviceQueueCreateInfo = {
@@ -192,13 +195,16 @@ static void process(spirv::ModuleOp module) {
 
     VkDevice device;
     BAIL_ON_BAD_RESULT(
-        vkCreateDevice(physicalDevices[i], &deviceCreateInfo, 0, &device));
+        vkCreateDevice(physicalDevices[0], &deviceCreateInfo, 0, &device));
+
     VkPhysicalDeviceMemoryProperties properties;
-    vkGetPhysicalDeviceMemoryProperties(physicalDevices[i], &properties);
+
+    vkGetPhysicalDeviceMemoryProperties(physicalDevices[0], &properties);
 
     const size_t K = 128;
     const int32_t bufferLength = K;
     const uint32_t bufferSize = sizeof(int32_t) * bufferLength;
+
     // we are going to need two buffers from this one memory
     const VkDeviceSize memorySize = bufferSize;
 
@@ -221,12 +227,218 @@ static void process(spirv::ModuleOp module) {
     BAIL_ON_BAD_RESULT(memoryTypeIndex == VK_MAX_MEMORY_TYPES
                            ? VK_ERROR_OUT_OF_HOST_MEMORY
                            : VK_SUCCESS);
+
     const VkMemoryAllocateInfo memoryAllocateInfo = {
         VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO, 0, memorySize, memoryTypeIndex};
+
+    // Allocate the device memory.
+    VkDeviceMemory memory1;
+    BAIL_ON_BAD_RESULT(
+        vkAllocateMemory(device, &memoryAllocateInfo, 0, &memory1));
+    VkDeviceMemory memory2;
+    BAIL_ON_BAD_RESULT(
+        vkAllocateMemory(device, &memoryAllocateInfo, 0, &memory2));
+    VkDeviceMemory memory3;
+    BAIL_ON_BAD_RESULT(
+        vkAllocateMemory(device, &memoryAllocateInfo, 0, &memory3));
+
+    // Map the device memory to host memory
+    int32_t *payload1, *payload2, *payload3;
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory1, 0, memorySize, 0, (void **)&payload1));
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory2, 0, memorySize, 0, (void **)&payload2));
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory3, 0, memorySize, 0, (void **)&payload3));
+
+    // Init 2D tensors.
+    for (int i = 0; i < K; ++i) {
+      payload2[i] = 1;
+      payload3[i] = 3;
+      payload1[i] = 0;
+    }
+    vkUnmapMemory(device, memory1);
+    vkUnmapMemory(device, memory2);
+    vkUnmapMemory(device, memory3);
+
+    const VkBufferCreateInfo bufferCreateInfo = {
+        VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        0,
+        0,
+        bufferSize,
+        VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
+        VK_SHARING_MODE_EXCLUSIVE,
+        1,
+        &queueFamilyIndex};
+
+    // Create buffers and bind them to the device memory.
+    VkBuffer buffer1, buffer2, buffer3;
+    BAIL_ON_BAD_RESULT(
+        vkCreateBuffer(device, &bufferCreateInfo, 0, &buffer1));
+    BAIL_ON_BAD_RESULT(vkBindBufferMemory(device, buffer1, memory1, 0));
+    BAIL_ON_BAD_RESULT(
+        vkCreateBuffer(device, &bufferCreateInfo, 0, &buffer2));
+    BAIL_ON_BAD_RESULT(vkBindBufferMemory(device, buffer2, memory2, 0));
+    BAIL_ON_BAD_RESULT(
+        vkCreateBuffer(device, &bufferCreateInfo, 0, &buffer3));
+    BAIL_ON_BAD_RESULT(vkBindBufferMemory(device, buffer3, memory3, 0));
+
+    // Read the shader from file.
+    size_t size = 0;
+    // Hardcoded path to binary shader.
+    uint32_t *shader_ptr = nullptr;
+
+    VkShaderModuleCreateInfo shaderModuleCreateInfo = {
+        VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO, 0, 0, size, shader_ptr};
+    VkShaderModule shader_module;
+    // Create Shader Module.
+    BAIL_ON_BAD_RESULT(vkCreateShaderModule(device, &shaderModuleCreateInfo, 0,
+                                            &shader_module));
+
+    VkDescriptorSetLayoutBinding descriptorSetLayoutBindings[3] = {
+        {0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT,
+         0},
+        {1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT,
+         0},
+        {2, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT,
+         0}};
+
+    // 3 buffers - 3 bindings
+    VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo = {
+        VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO, 0, 0, 3,
+        descriptorSetLayoutBindings};
+
+    VkDescriptorSetLayout descriptorSetLayout;
+    BAIL_ON_BAD_RESULT(vkCreateDescriptorSetLayout(
+        device, &descriptorSetLayoutCreateInfo, 0, &descriptorSetLayout));
+
+    VkPipelineLayoutCreateInfo pipelineLayoutCreateInfo = {
+        VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+        0,
+        0,
+        1,
+        &descriptorSetLayout,
+        0,
+        0};
+
+    VkPipelineLayout pipelineLayout;
+    BAIL_ON_BAD_RESULT(vkCreatePipelineLayout(device, &pipelineLayoutCreateInfo,
+                                              0, &pipelineLayout));
+
+    const char *kernel_name = "compute_kernel";
+
+    VkComputePipelineCreateInfo computePipelineCreateInfo = {
+        VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
+        0,
+        0,
+        {VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO, 0, 0,
+         VK_SHADER_STAGE_COMPUTE_BIT, shader_module, kernel_name, 0},
+        pipelineLayout,
+        0,
+        0};
+
+    VkPipeline pipeline;
+
+    BAIL_ON_BAD_RESULT(vkCreateComputePipelines(
+        device, 0, 1, &computePipelineCreateInfo, 0, &pipeline));
+
+    VkCommandPoolCreateInfo commandPoolCreateInfo = {
+        VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO, 0, 0, queueFamilyIndex};
+
+    VkDescriptorPoolSize descriptorPoolSize = {
+        VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 3};
+
+    VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = {
+        VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
+        0,
+        0,
+        1,
+        1,
+        &descriptorPoolSize};
+
+    VkDescriptorPool descriptorPool;
+    BAIL_ON_BAD_RESULT(vkCreateDescriptorPool(device, &descriptorPoolCreateInfo,
+                                              0, &descriptorPool));
+
+    VkDescriptorSetAllocateInfo descriptorSetAllocateInfo = {
+        VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO, 0, descriptorPool, 1,
+        &descriptorSetLayout};
+
+    VkDescriptorSet descriptorSet;
+    BAIL_ON_BAD_RESULT(vkAllocateDescriptorSets(
+        device, &descriptorSetAllocateInfo, &descriptorSet));
+
+    VkDescriptorBufferInfo in1_descriptorBufferInfo = {buffer1, 0,
+                                                       VK_WHOLE_SIZE};
+    VkDescriptorBufferInfo in2_descriptorBufferInfo = {buffer2, 0,
+                                                       VK_WHOLE_SIZE};
+    VkDescriptorBufferInfo in3_descriptorBufferInfo = {buffer3, 0,
+                                                       VK_WHOLE_SIZE};
+
+    const int descriptors_count = 3;
+
+    VkWriteDescriptorSet writeDescriptorSet[descriptors_count] = {
+        {VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET, 0, descriptorSet, 0, 0, 1,
+         VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 0, &in1_descriptorBufferInfo, 0},
+        {VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET, 0, descriptorSet, 1, 0, 1,
+         VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 0, &in2_descriptorBufferInfo, 0},
+        {VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET, 0, descriptorSet, 2, 0, 1,
+         VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 0, &in3_descriptorBufferInfo, 0}};
+
+    vkUpdateDescriptorSets(device, descriptors_count, writeDescriptorSet, 0, 0);
+
+    VkCommandPool commandPool;
+    BAIL_ON_BAD_RESULT(
+        vkCreateCommandPool(device, &commandPoolCreateInfo, 0, &commandPool));
+
+    VkCommandBufferAllocateInfo commandBufferAllocateInfo = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO, 0, commandPool,
+        VK_COMMAND_BUFFER_LEVEL_PRIMARY, 1};
+
+    VkCommandBuffer commandBuffer;
+    BAIL_ON_BAD_RESULT(vkAllocateCommandBuffers(
+        device, &commandBufferAllocateInfo, &commandBuffer));
+
+    VkCommandBufferBeginInfo commandBufferBeginInfo = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO, 0,
+        VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT, 0};
+
+    BAIL_ON_BAD_RESULT(
+        vkBeginCommandBuffer(commandBuffer, &commandBufferBeginInfo));
+
+    vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline);
+
+    vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE,
+                            pipelineLayout, 0, 1, &descriptorSet, 0, 0);
+
+    vkCmdDispatch(commandBuffer, 1, 1, 1);
+
+    BAIL_ON_BAD_RESULT(vkEndCommandBuffer(commandBuffer));
+
+    VkQueue queue;
+    vkGetDeviceQueue(device, queueFamilyIndex, 0, &queue);
+
+    VkSubmitInfo submitInfo = {
+        VK_STRUCTURE_TYPE_SUBMIT_INFO, 0, 0, 0, 0, 1, &commandBuffer, 0, 0};
+
+    BAIL_ON_BAD_RESULT(vkQueueSubmit(queue, 1, &submitInfo, 0));
+
+    BAIL_ON_BAD_RESULT(vkQueueWaitIdle(queue));
+
+    // Check the result.
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory1, 0, memorySize, 0, (void **)&payload1));
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory2, 0, memorySize, 0, (void **)&payload2));
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory3, 0, memorySize, 0, (void **)&payload3));
   }
 }
 
-static LogicalResult runOnModule(raw_ostream &os, ModuleOp module) {
+static LogicalResult
+runOnModule(raw_ostream &os, ModuleOp module,
+            std::unordered_map<int, std::vector<int32_t>> &vars) {
+
   if (failed(module.verify())) {
     return failure();
   }
@@ -239,13 +451,20 @@ static LogicalResult runOnModule(raw_ostream &os, ModuleOp module) {
         spirvModule.emitError("found more than one module");
       }
       done = true;
-      process(spirvModule);
+      processModule(spirvModule, vars);
     });
   }
 
   // out the result
   module.print(os);
   return success();
+}
+
+static void PopulateData(std::unordered_map<int, std::vector<int32_t>> &vars) {
+  std::vector<int32_t> data = {1, 2, 3, 4};
+  for (int i = 0; i < 3; ++i) {
+    vars.insert({i, move(data)});
+  }
 }
 
 int main(int argc, char **argv) {
@@ -268,6 +487,8 @@ int main(int argc, char **argv) {
 
   SourceMgr sourceMgr;
   sourceMgr.AddNewSourceBuffer(std::move(inputFile), SMLoc());
+  std::unordered_map<int, std::vector<int32_t>> variables;
+  PopulateData(variables);
 
   MLIRContext context;
   OwningModuleRef moduleRef(parseSourceFile(sourceMgr, &context));
@@ -276,7 +497,7 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  if (failed(runOnModule(outputFile->os(), moduleRef.get()))) {
+  if (failed(runOnModule(outputFile->os(), moduleRef.get(), variables))) {
     llvm::errs() << "can't run on module" << '\n';
     return 1;
   }
