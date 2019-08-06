bbccbaded997c5c618bae5e891039bfec6b14e8b
Denis Khalikov <dennis.khalikov@gmail.com>
2019-08-06 19:18:18 +0300

    Device creation

--- tools/mlir-vulkan-runner/mlir-vulkan-runner.cpp
+++ tools/mlir-vulkan-runner/mlir-vulkan-runner.cpp
@@ -43,9 +43,17 @@
 #include "llvm/Support/ToolOutputFile.h"
 
 #include <iostream>
+#include <vulkan/vulkan.h>
 
 using namespace mlir;
 using namespace llvm;
+
+#define BAIL_ON_BAD_RESULT(result)                                             \
+  if (VK_SUCCESS != (result)) {                                                \
+    fprintf(stderr, "Failure at %u %s\n", __LINE__, __FILE__);                 \
+    fprintf(stderr, "Exit code %d\n", result);                                 \
+    exit(-1);                                                                  \
+  }
 
 static cl::opt<std::string>
     inputFilename(cl::Positional, cl::desc("<input file>"), cl::init("-"));
@@ -53,6 +61,77 @@ static cl::opt<std::string>
 static cl::opt<std::string> outputFilename("o", cl::desc("Output filename"),
                                            cl::value_desc("filename"),
                                            cl::init("-"));
+
+static void *_alloca(size_t size) { return malloc(size); }
+
+static VkResult vkGetBestComputeQueueNPH(VkPhysicalDevice physicalDevice,
+                                         uint32_t *queueFamilyIndex) {
+  uint32_t queueFamilyPropertiesCount = 0;
+  vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice,
+                                           &queueFamilyPropertiesCount, 0);
+
+  VkQueueFamilyProperties *const queueFamilyProperties =
+      (VkQueueFamilyProperties *)_alloca(sizeof(VkQueueFamilyProperties) *
+                                         queueFamilyPropertiesCount);
+
+  vkGetPhysicalDeviceQueueFamilyProperties(
+      physicalDevice, &queueFamilyPropertiesCount, queueFamilyProperties);
+
+  // first try and find a queue that has just the compute bit set
+  for (uint32_t i = 0; i < queueFamilyPropertiesCount; i++) {
+    // mask out the sparse binding bit that we aren't caring about (yet!) and
+    // the transfer bit
+    const VkQueueFlags maskedFlags =
+        (~(VK_QUEUE_TRANSFER_BIT | VK_QUEUE_SPARSE_BINDING_BIT) &
+         queueFamilyProperties[i].queueFlags);
+
+    if (!(VK_QUEUE_GRAPHICS_BIT & maskedFlags) &&
+        (VK_QUEUE_COMPUTE_BIT & maskedFlags)) {
+      *queueFamilyIndex = i;
+      return VK_SUCCESS;
+    }
+  }
+
+  // lastly get any queue that'll work for us
+  for (uint32_t i = 0; i < queueFamilyPropertiesCount; i++) {
+    // mask out the sparse binding bit that we aren't caring about (yet!) and
+    // the transfer bit
+    const VkQueueFlags maskedFlags =
+        (~(VK_QUEUE_TRANSFER_BIT | VK_QUEUE_SPARSE_BINDING_BIT) &
+         queueFamilyProperties[i].queueFlags);
+
+    if (VK_QUEUE_COMPUTE_BIT & maskedFlags) {
+      *queueFamilyIndex = i;
+      return VK_SUCCESS;
+    }
+  }
+
+  return VK_ERROR_INITIALIZATION_FAILED;
+}
+
+static const VkInstance vulkanCreateInstance() {
+  const VkApplicationInfo applicationInfo = {VK_STRUCTURE_TYPE_APPLICATION_INFO,
+                                             0,
+                                             "VKComputeSample",
+                                             0,
+                                             "",
+                                             0,
+                                             VK_MAKE_VERSION(1, 0, 9)};
+
+  const VkInstanceCreateInfo instanceCreateInfo = {
+      VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+      0,
+      0,
+      &applicationInfo,
+      0,
+      0,
+      0,
+      0};
+
+  VkInstance instance;
+  BAIL_ON_BAD_RESULT(vkCreateInstance(&instanceCreateInfo, 0, &instance));
+  return instance;
+}
 
 static void processVariable(spirv::VariableOp varOp) {
   auto descriptorSetName =
@@ -72,6 +151,78 @@ static void process(spirv::ModuleOp module) {
     if (isa<spirv::VariableOp>(op)) {
       processVariable(dyn_cast<spirv::VariableOp>(op));
     }
+  }
+
+  const VkInstance instance = vulkanCreateInstance();
+  uint32_t physicalDeviceCount = 0;
+  BAIL_ON_BAD_RESULT(
+      vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, 0));
+
+  VkPhysicalDevice *const physicalDevices = (VkPhysicalDevice *)malloc(
+      sizeof(VkPhysicalDevice) * physicalDeviceCount);
+
+  BAIL_ON_BAD_RESULT(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount,
+                                                physicalDevices));
+
+  for (uint32_t i = 0; i < physicalDeviceCount; i++) {
+    uint32_t queueFamilyIndex = 0;
+    BAIL_ON_BAD_RESULT(
+        vkGetBestComputeQueueNPH(physicalDevices[i], &queueFamilyIndex));
+
+    const float queuePrioritory = 1.0f;
+    const VkDeviceQueueCreateInfo deviceQueueCreateInfo = {
+        VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        0,
+        0,
+        queueFamilyIndex,
+        1,
+        &queuePrioritory};
+
+    const VkDeviceCreateInfo deviceCreateInfo = {
+        VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        0,
+        0,
+        1,
+        &deviceQueueCreateInfo,
+        0,
+        0,
+        0,
+        0,
+        0};
+
+    VkDevice device;
+    BAIL_ON_BAD_RESULT(
+        vkCreateDevice(physicalDevices[i], &deviceCreateInfo, 0, &device));
+    VkPhysicalDeviceMemoryProperties properties;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevices[i], &properties);
+
+    const size_t K = 128;
+    const int32_t bufferLength = K;
+    const uint32_t bufferSize = sizeof(int32_t) * bufferLength;
+    // we are going to need two buffers from this one memory
+    const VkDeviceSize memorySize = bufferSize;
+
+    // set memoryTypeIndex to an invalid entry in the properties.memoryTypes
+    // array
+    uint32_t memoryTypeIndex = VK_MAX_MEMORY_TYPES;
+
+    for (uint32_t k = 0; k < properties.memoryTypeCount; k++) {
+      if ((VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT &
+           properties.memoryTypes[k].propertyFlags) &&
+          (VK_MEMORY_PROPERTY_HOST_COHERENT_BIT &
+           properties.memoryTypes[k].propertyFlags) &&
+          (memorySize <
+           properties.memoryHeaps[properties.memoryTypes[k].heapIndex].size)) {
+        memoryTypeIndex = k;
+        break;
+      }
+    }
+
+    BAIL_ON_BAD_RESULT(memoryTypeIndex == VK_MAX_MEMORY_TYPES
+                           ? VK_ERROR_OUT_OF_HOST_MEMORY
+                           : VK_SUCCESS);
+    const VkMemoryAllocateInfo memoryAllocateInfo = {
+        VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO, 0, memorySize, memoryTypeIndex};
   }
 }
 
